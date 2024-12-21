a21aea620df3bb244b7086818fedd0fff920c2f7
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2024-12-21 16:28:54 +0100

    Update Readme.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import LoginPage from "./pages/auth/login/page";
-import RegisterPage from "./pages/auth/register/page";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome</h1>
-      <LoginPage/>
-      <RegisterPage/>
-
+     
     </div>
     
   );
